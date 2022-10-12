30cc3bf6a0e3fdb984968a5861d34c2935705de1
Erin Chon <echon@bsd220.org>
2022-10-12 09:51:54 -0500

    Initial Commit

---
+++ final grade calculator/final_grade_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_grade_calculatorApp.swift
+//  final grade calculator
+//
+//  Created by Erin Chon on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct final_grade_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ final grade calculatorUITests/final_grade_calculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  final_grade_calculatorUITestsLaunchTests.swift
+//  final grade calculatorUITests
+//
+//  Created by Erin Chon on 9/27/22.
+//
+
+import XCTest
+
+final class final_grade_calculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
